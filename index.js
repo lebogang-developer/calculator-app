8e8f9284769fda3cff33ea85b7efc2a6602916a2
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2025-06-10 11:36:43 +0200

    Removing the 0 on the display screen

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-
-
 // Event handler for digits/numbers
 function digitBtnPressed(button) {
   // Store the current value
-//   var currentValue = document.getElementById('inputBox').value;
-  document.getElementById('inputBox').value += button.textContent;
+  // var currentValue = document.getElementById('inputBox').value;
+  // document.getElementById("inputBox").value += button.textContent;
+  const input = document.getElementById('inputBox');
+  const currentValue = input.value;
+  const newDigit = button.textContent;
+
+  input.value = currentValue === '0' ? newDigit : currentValue + newDigit;
 }
